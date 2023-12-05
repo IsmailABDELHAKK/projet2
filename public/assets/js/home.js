cf65b5936a71979fd4216e3eb0aa37a453e400b3
CamilleUras <camille.uras@outlook.fr>
2023-12-05 14:21:45 +0100

    fixe du css

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -5,6 +5,7 @@ console.log('Tableau des membres de l\'équipe : ', team);
 // TODO: Affichage des destinations dans la section #destinations de la page d'accueil
 // Appel de la fonction getDestinations
 // getDestinations(destinations);
+getDestinations(destinations);
 
 // Affichage des membres de l'équipe dans la section #about de la page d'accueil
 // Appel de la fonction getTeam
